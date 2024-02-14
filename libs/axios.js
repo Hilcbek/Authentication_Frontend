df30bb7052ace1b683894982a4092d56bdd1d226
Hilcbek <balemayehu07@gmail.com>
2024-02-14 22:45:15 +0300

    adadaasdsa

--- libs/axios.js
+++ libs/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export let baseUrl = axios.create({
-    baseURL : "http://localhost:5000/api",
-    withCredentials : true
-})
+  baseURL: "https://auth-mern-rldb.onrender.com/api",
+  withCredentials: true,
+});
